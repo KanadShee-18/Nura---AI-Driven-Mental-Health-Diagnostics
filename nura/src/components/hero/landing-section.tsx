205553a51ad7f38a4c6e35cc35a5852b6bb27344
Kanad Shee <codewizardkanad@gmail.com>
2025-12-14 20:06:51 +0530

    fix: Modified some visuals

--- nura/src/components/hero/landing-section.tsx
+++ nura/src/components/hero/landing-section.tsx
@@ -7,7 +7,7 @@ import { motion } from "motion/react";
 
 export const LandingHero = () => {
   return (
-    <section id='landing-hero' className='pt-14'>
+    <section id='landing-hero' className='pt-14 md:pt-20'>
       <Container className='px-5 md:px-10 flex flex-col gap-y-7 items-center justify-center pt-10'>
         <motion.h2
           initial={{ opacity: 0, filter: "blur(7px)", y: 10 }}
